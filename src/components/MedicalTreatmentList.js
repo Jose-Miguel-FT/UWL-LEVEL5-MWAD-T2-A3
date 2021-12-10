ec3a208019029d480c4eb696818f5e45b77f331e
Jose-Miguel-FT <miguelfinol_48@hotmail.com>
2021-12-10 14:54:23 +0000

    1 file modified

--- src/components/MedicalTreatmentList.js
+++ src/components/MedicalTreatmentList.js
@@ -4,25 +4,49 @@ function MedicalTreatmentList(props) {
   const treatmentsJson = props.string;
   const medicalTreatmentsArray = JSON.parse(treatmentsJson);
 
-  const listMedicalTreats = medicalTreatmentsArray.map((medicalTreatment) => (
+  const [box, setBox] = React.useState('');
+
+  const listMedicalTreats = medicalTreatmentsArray.map((item) => (
     <li>
-      Treat ID: {medicalTreatment.treatId}
-      <br />
-      Treat Course ID: {medicalTreatment.treatCourseId}
-      <br />
-      Type: {medicalTreatment.type}
-      <br />
-      Category: {medicalTreatment.category}
-      <br />
-      Name: {medicalTreatment.name}
-      <br />
-      Start Date: {medicalTreatment.startDate}
-      <br />
+      Record {item.treatId} details:
+      <div>
+        Course ID = {item.treatCourseId}
+        <br />
+        Type = {item.type}
+        <br />
+        Category = {item.category}
+        <br />
+        Name = {item.name}
+        <br />
+        Date = {item.startDate}
+      </div>
       <br />
     </li>
   ));
 
-  return <div>{listMedicalTreats}</div>;
+  const filtering = medicalTreatmentsArray
+    .filter((m) => m.category.includes('Nurse'))
+    .map((filteredElement) => (
+      <li>
+        Treat ID: {filteredElement.treatId}
+        <br />
+        Course ID: {filteredElement.treatCourseId}
+        <br />
+        Type: {filteredElement.type}
+        <br />
+        Name: {filteredElement.name}
+        <br />
+        <br />
+      </li>
+    ));
+
+  return (
+    <div>
+      {listMedicalTreats}
+      <p>CATEGORY: Nurse</p>
+      {filtering}
+    </div>
+  );
 }
 
 export default MedicalTreatmentList;
